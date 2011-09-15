abc46e6a0013b9c130812e2a81579dddb0c416a6
Yuhei Kagaya <yuhei.kagaya@gmail.com>
2011-09-15 20:13:27 +0900

    環境構築一発作成スクリプト追加。

---
+++ init_my_home.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd ~
+mkdir swap backup
+ln -sf ~/dotfiles/.bashrc .bashrc
+ln -sf ~/dotfiles/.vimrc .vimrc
+ln -sf ~/dotfiles/.vim .vim
+ln -sf ~/dotfiles/.ctags .ctags
+ln -sf ~/dotfiles/.tmux.conf .tmux.conf
+ln -sf ~/dotfiles/.gitignore .gitignore
+#ln -sf ~/dotfiles/.gitconfig .gitconfig
+
+cd ~/dotfiles/
+git submodule init
+git submodule update
+
+cd ~
