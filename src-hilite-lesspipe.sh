05d8877c251df81660578b936a016a34a730e930
violetyk <yuhei.kagaya@nanapi.co.jp>
2014-02-18 15:14:16 +0900

    Add setting

--- src-hilite-lesspipe.sh
+++ src-hilite-lesspipe.sh
@@ -3,11 +3,11 @@
 for source in "$@"; do
   case $source in
     *ChangeLog|*changelog) 
-      source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=esc.style -i "$source" ;;
+      source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=$HOME/dotfiles/source-highlight/wombat.style -i "$source" ;;
     *Makefile|*makefile) 
-      source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=esc.style -i "$source" ;;
+      source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=$HOME/dotfiles/source-highlight/wombat.style -i "$source" ;;
     *query.log|*slow.log) 
-      source-highlight --failsafe -f esc --lang-def=sql.lang --style-file=esc.style -i "$source" ;;
-    *) source-highlight --failsafe --infer-lang -f esc --style-file=esc.style -i "$source" ;;
+      source-highlight --failsafe -f esc --lang-def=sql.lang --style-file=$HOME/dotfiles/source-highlight/wombat.style -i "$source" ;;
+    *) source-highlight --failsafe --infer-lang -f esc --style-file=$HOME/dotfiles/source-highlight/wombat.style -i "$source" ;;
   esac
 done
