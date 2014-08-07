d48a0f53094de28944a5361839117da69c883b85
violetyk <yuhei.kagaya@gmail.com>
2014-08-07 13:37:26 +0900

    Move dotfiles

--- setup.sh
+++ setup.sh
@@ -7,17 +7,17 @@ cd ~/etc/profile.d
 curl -S -o ~/etc/profile.d/git-prompt.sh https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
 curl -S -o ~/etc/profile.d/git-completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
 
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.profile ~/.profile
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.ctags ~/.ctags
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.tigrc ~/.tigrc
-ln -sf ~/dotfiles/.agignore ~/.agignore
-ln -sf ~/dotfiles/.gitignore ~/.gitignore
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.vim/bundle/dircolors-solarized/dircolors.ansi-universal ~/.dircolors
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/profile ~/.profile
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim ~/.vim
+ln -sf ~/dotfiles/ctags ~/.ctags
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tigrc ~/.tigrc
+ln -sf ~/dotfiles/agignore ~/.agignore
+ln -sf ~/dotfiles/gitignore ~/.gitignore
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/vim/bundle/dircolors-solarized/dircolors.ansi-universal ~/.dircolors
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
 
 cd ~/.vim/manual
@@ -58,6 +58,6 @@ fi
 
 
 cd ~/dotfiles/
-git clone git://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
-git clone git://github.com/seebi/dircolors-solarized ~/dotfiles/.vim/bundle/dircolors-solarized
+git clone git://github.com/Shougo/neobundle.vim ~/dotfiles/vim/bundle/neobundle.vim
+git clone git://github.com/seebi/dircolors-solarized ~/dotfiles/vim/bundle/dircolors-solarized
 vim  +NeoBundleInstall +q
