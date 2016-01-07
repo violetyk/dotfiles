283d4dfc8686c0035d983bed892d14f3578509cf
violetyk <yuhei.kagaya@gmail.com>
2016-01-07 12:53:16 +0900

    source-highlightを整理

---
+++ playbooks/roles/home/files/.source-highlight/src-hilite-lesspipe.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+for source in "$@"; do
+  case $source in
+    *ChangeLog|*changelog) 
+      source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=$HOME/.source-highlight/wombat.style -i "$source" ;;
+    *Makefile|*makefile) 
+      source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=$HOME/.source-highlight/wombat.style -i "$source" ;;
+    *query.log|*slow.log) 
+      source-highlight --failsafe -f esc --lang-def=sql.lang --style-file=$HOME/.source-highlight/wombat.style -i "$source" ;;
+    *) source-highlight --failsafe --infer-lang -f esc --style-file=$HOME/.source-highlight/wombat.style -i "$source" ;;
+  esac
+done
